2e4dc75af963c9996ce1665ebf812fb5ecd2a5cb
Alanoud Aloraydi <alanoudkh16@gmail.com>
2026-08-24 11:18:00 +0300

    feat: add annotated floor helper

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Provide a function that calculates the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the greatest integer that is less than or equal to n."""
+    return math.floor(n)
