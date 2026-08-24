2fb4f9bc4f5fed5974ff259b4c99a37e697129c4
Alanoud Aloraydi <alanoudkh16@gmail.com>
2026-08-24 16:01:00 +0300

    feat: add typed float list summation

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Provide a function for summing a list of floats."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of all floating-point values in input_list."""
+    return sum(input_list)
