544ab88e7013f2cd14f4f91677b58f84d1d0f43b
Alanoud Aloraydi <alanoudkh16@gmail.com>
2026-08-28 13:36:00 +0300

    feat: add random async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Provide an asynchronous generator of random floating-point values."""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield ten random floats after one-second asynchronous delays."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
